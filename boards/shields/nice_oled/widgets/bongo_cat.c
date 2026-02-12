9881af4566a6be092b8a02efb8584a7f2d413eaf
0kenx <km@nxfi.app>
2026-02-12 12:35:43 +0100

    migrate to lvgl 9
    Signed-off-by: 0kenx <km@nxfi.app>

--- boards/shields/nice_oled/widgets/bongo_cat.c
+++ boards/shields/nice_oled/widgets/bongo_cat.c
@@ -62,24 +62,24 @@ LV_IMG_DECLARE(bongo_cat_tap2_03);
 LV_IMG_DECLARE(bongo_cat_tap2_04);
 
 #define ANIMATION_SPEED_IDLE 10000
-const lv_img_dsc_t *idle_imgs[] = {
+static const lv_img_dsc_t *idle_imgs[] = {
     &bongo_cat_double_tap1_06,
 };
 
 #define ANIMATION_SPEED_SLOW 2000
-const lv_img_dsc_t *slow_imgs[] = {
+static const lv_img_dsc_t *slow_imgs[] = {
     &bongo_cat_tap1_03,
     &bongo_cat_tap2_03,
 };
 
 #define ANIMATION_SPEED_MID 500
-const lv_img_dsc_t *mid_imgs[] = {
+static const lv_img_dsc_t *mid_imgs[] = {
     &bongo_cat_tap1_03,
     &bongo_cat_tap2_03,
 };
 
 #define ANIMATION_SPEED_FAST 200
-const lv_img_dsc_t *fast_imgs[] = {
+static const lv_img_dsc_t *fast_imgs[] = {
     &bongo_cat_double_tap2_02,
     &bongo_cat_double_tap1_03,
 };
@@ -94,7 +94,8 @@ enum anim_state {
     anim_state_slow,
     anim_state_mid,
     anim_state_fast
-} current_anim_state;
+};
+static enum anim_state current_anim_state;
 
 static void set_animation(lv_obj_t *animing, struct wpm_bongo_cat_status_state state) {
     if (state.wpm < 5) {
